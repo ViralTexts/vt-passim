5b60d080917b340b5fac30b2f00ad2c2f8fa5363
David Smith <dasmiq@gmail.com>
2015-09-27 07:02:41 -0400

    Add ca2json.pl from chroniclingamerica.

---
+++ bin/ca2json.pl
@@ -0,0 +1,89 @@
+#!/usr/bin/perl
+
+use strict;
+use File::Basename;
+use Getopt::Std;
+
+my %opts = ();
+getopts('t:d:', \%opts);
+
+my $curid = '';
+my $hypflag = 0;
+my($sn, $year, $month, $day, $ed, $seq);
+while (<>) {
+    chomp;
+    if ( /^[^<].*\.xml$/ ) {
+	($sn, $year, $month, $day, $ed, $seq) = split /\//;
+	my $date = "$year-$month-$day";
+	my $id = join('/', $sn, $date, $ed, $seq);
+	my $issue = join('/', $sn, $date, $ed);
+	$ed =~ s/^ed\-//;
+	$seq =~ s/^seq\-//;
+	
+	my $url = "http://chroniclingamerica.loc.gov/lccn/$id";
+	
+	if ( $curid ne $id ) {
+	    if ( $curid ne '' ) {
+		print "\"}\n";
+	    }
+	    $curid = $id;
+	    print "{\n";
+	    print "\"id\": \"$id\",\n";
+	    print "\"series\": \"$sn\",\n";
+	    print "\"issue\": \"$issue\",\n";
+	    print "\"date\": \"$date\",\n";
+	    print "\"ed\": \"$ed\",\n";
+	    print "\"seq\": \"$seq\",\n";
+	    print "\"url\": \"$url\",\n";
+	    print "\"text\": \"";
+	}
+    
+	# print "<pb n=\"$seq\"/>\n";
+	next;
+    }
+
+    while ( /(<\/?[A-Za-z][^>]*>)/g ) {
+	my $t = $1;
+	
+	if ( $t =~ /^<String / ) {
+	    my($h) = ( $t =~ / HEIGHT=[\'\"]([0-9]+)/ );
+	    my($w) = ( $t =~ / WIDTH=[\'\"]([0-9]+)/ );
+	    my($x) = ( $t =~ / HPOS=[\'\"]([0-9]+)/ );
+	    my($y) = ( $t =~ / VPOS=[\'\"]([0-9]+)/ );
+	    print "<w coords=\\\"$x,$y,$w,$h\\\"/>" unless $hypflag;
+	    if ( $t =~ / CONTENT=\"([^\"]+)\"/ ) {
+		print jsesc($1);
+	    } elsif ( $t =~ / CONTENT=\'([^\']+)\'/ ) {
+		print jsesc($1);
+	    }
+	    if ( $hypflag ) {
+		print "\n";
+		$hypflag = 0;
+	    }
+	} elsif ( $t =~ /^<SP / ) {
+	    print " ";
+	} elsif ( $t =~ /^<\/TextLine>/ ) {
+	    print "\n" unless $hypflag;
+	} elsif ( $t =~ /^<HYP /) {
+	    $hypflag = 1;
+	} elsif ( $t =~ /^<\/TextBlock>/ ) {
+	    print "\n";
+	    $hypflag = 0;
+	}
+    }
+    
+
+    # ## Some documents transcribe hyphens as the logical not character
+    # s/[ ]*\xc2\xac[ ]*\n[ ]*(\S+)[ ]+/$1\n/g;
+}
+
+if ( $curid ne '' ) {
+    print "\"}\n"
+}
+
+sub jsesc {
+    my($s) = @_;
+    $s =~ s/\\/\\\\/g;
+    $s =~ s/\"/\\"/g;
+    $s;
+}
