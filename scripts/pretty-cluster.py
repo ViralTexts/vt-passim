8d0b829395264519dc41902da8a353a2599fcfc5
David Smith <dasmiq@gmail.com>
2016-03-28 15:40:51 -0400

    Use join instead of dict for metadata.

--- scripts/pretty-cluster.py
+++ scripts/pretty-cluster.py
@@ -6,8 +6,18 @@ import HTMLParser
 
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
-from pyspark.sql.functions import udf, array_contains, explode, desc
+from pyspark.sql.functions import col, udf, array_contains, explode, desc
 from pyspark.sql.types import StringType
+
+def guessFormat(path, default="json"):
+    if path.endswith(".json"):
+        return ("json", {})
+    elif path.endswith(".parquet"):
+        return ("parquet", {})
+    elif path.endswith(".csv"):
+        return ("com.databricks.spark.csv", {'header': 'true'})
+    else:
+        return (default, {})
 
 def formatURL(url, corpus, id, pages, regions):
     if corpus == 'ca':
@@ -22,42 +32,36 @@ def formatURL(url, corpus, id, pages, regions):
         return url
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: pretty-cluster.py <metadata> <input> <output>", file=sys.stderr)
+    if len(sys.argv) < 4:
+        print("Usage: pretty-cluster.py <metadata> <input> <output> [<query>]", file=sys.stderr)
         exit(-1)
-    sc = SparkContext(appName="Parquet Load")
+    sc = SparkContext(appName="Prettyprint Clusters")
     sqlContext = SQLContext(sc)
 
-    # outputFormat = "com.databricks.spark.csv"
-    # outputOptions = {'header': 'true'}
-    outputFormat = "json"
-    outputOptions = {}
+    outpath = sys.argv[3]
+    (outputFormat, outputOptions) = guessFormat(outpath, "json")
+
+    meta = sqlContext.read.json(sys.argv[1])\
+           .withColumnRenamed('lang', 'series_lang')\
+           .withColumnRenamed('id', 'series').dropDuplicates(['series'])
     
-    meta = sqlContext.read.json(sys.argv[1])
-
     raw = sqlContext.read.load(sys.argv[2])
-
-    ## There aren't many series.  It'll be cleaner to use a closure instead of a join
-    stitle = dict(meta.select(explode(meta.publication_names.sn).alias("series"),
-                             meta.master_name)\
-                  .unionAll(meta.select(meta.id.alias("series"), meta.master_name))\
-                  .distinct().collect())
 
     h = HTMLParser.HTMLParser()
     removeTags = udf(lambda s: h.unescape(sub("</?[A-Za-z][^>]*>", "", s)), StringType())
     constructURL = udf(lambda url, corpus, id, pages, regions: formatURL(url, corpus, id, pages, regions),
                        StringType())
-    getTitle = udf(lambda series: stitle[series] if series in stitle else series, StringType())
+
     raw\
         .withColumnRenamed("title", "heading")\
-        .withColumn("title", getTitle(raw.series))\
-        .withColumn("text", removeTags(raw.text))\
         .withColumn("url", constructURL(raw.url, raw.corpus, raw.id, raw.pages, raw.regions))\
         .drop("locs")\
         .drop("pages")\
         .drop("regions")\
+        .withColumn("text", removeTags(raw.text))\
+        .join(meta, 'series', 'left_outer')\
         .orderBy(desc("size"), "cluster", "date", "id", "begin")\
-        .write.format(outputFormat).options(**outputOptions).save(sys.argv[3])
+        .write.format(outputFormat).options(**outputOptions).save(outpath)
 
     sc.stop()
     
