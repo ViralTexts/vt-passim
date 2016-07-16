8960604436820146faaab2b54a1622a01186e2e0
David Smith <dasmiq@gmail.com>
2016-07-15 21:42:09 -0400

    Get coordinate information

--- src/main/scala/MetsAlto.scala
+++ src/main/scala/MetsAlto.scala
@@ -1,24 +1,15 @@
 package vtpassim
 
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
-import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.sql.SQLContext
-import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.Row
-import org.apache.spark.sql.functions._
-import org.apache.spark.storage.StorageLevel
-
-import org.apache.hadoop.fs.{FileSystem,Path}
-
-import org.apache.spark.sql.catalyst.util.DateTimeUtils
-import java.sql.Date
 
 import collection.JavaConversions._
-import scala.collection.mutable.ListBuffer
-import scala.collection.mutable.ArrayBuffer
-import scala.collection.mutable.Queue
+import scala.collection.mutable.{ArrayBuffer, StringBuilder}
+
+import vtpassim.pageinfo._
+
+case class Record(id: String, issue: String, series: String, date: String, lang: String,
+  dpi: Int, text: String, regions: Array[Region])
 
 object MetsAlto {
   def main(args: Array[String]) {
@@ -38,34 +29,41 @@ object MetsAlto {
 
           val zfile = new java.util.zip.ZipFile(fname)
           import scala.collection.JavaConversions._
-          val (series, date, lang) =
+          val (series, date, lang, dpi) =
             try {
               val mfile = zfile.entries.filter(_.getName.endsWith("mets.xml")).toSeq.head
               val t = scala.xml.XML.load(zfile.getInputStream(mfile))
               ((t \\ "identifier").head.text,
                 (t \\ "dateIssued").head.text
                   .replaceAll("""^(\d\d)\.(\d\d)\.(\d\d\d\d)$""", """$3-$2-$1"""),
-                (t \\ "languageTerm").head.text)
+                (t \\ "languageTerm").head.text,
+                (t \\ "XphysScanResolution").head.text.toInt)
             } catch {
-              case e: Exception => ("", "", "")
+              case e: Exception => ("", "", "", 0)
             }
 
           zfile.entries.filter(f => f.getName.endsWith(".xml") && !f.getName.endsWith("mets.xml"))
             .map(f => {
               val t = scala.xml.XML.load(zfile.getInputStream(f))
-              val res = (t \\ "TextLine").map { line =>
-                (line \ "_").map({ e =>
+              val buf = new StringBuilder
+              val regions = new ArrayBuffer[Region]
+              (t \\ "TextLine").foreach { line =>
+                (line \ "_").foreach({ e =>
                   if ( e.label == "String" ) {
-                    (e \ "@CONTENT").text.replaceAll("<", "&lt;")
+                    val start = buf.size
+                    buf.append((e \ "@CONTENT").text.replaceAll("&", "&amp;").replaceAll("<", "&lt;"))
+                    regions += Region(start, buf.size - start,
+                      Coords((e \ "@HPOS").text.toInt, (e \ "@VPOS").text.toInt,
+                        (e \ "@WIDTH").text.toInt, (e \ "@HEIGHT").text.toInt, (e \ "@HEIGHT").text.toInt))
                   } else if ( e.label == "SP" ) {
-                    " "
+                    buf.append(" ")
                   } else if ( e.label == "HYP" ) {
-                    "\u00ad"
+                    buf.append("\u00ad")
                   }
                 })
-                  .mkString("")
-            }
-              (f.getName.replaceAll(".xml$", ""), issue, series, date, lang, res.mkString("\n"))
+                buf.append("\n")
+              }
+              Record(f.getName.replaceAll(".xml$", ""), issue, series, date, lang, dpi, buf.toString, regions.toArray)
             })
         } catch {
           case e: Exception => {
@@ -75,7 +73,7 @@ object MetsAlto {
         }
       }
     )
-      .toDF("id", "issue", "series", "date", "lang", "text")
+      .toDF
       .write.save(args(1))
   }
 }
