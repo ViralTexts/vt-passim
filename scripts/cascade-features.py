2c79e9af2b79e0e71e2613965003c2ddfdf20dd7
David Smith <dasmiq@gmail.com>
2016-08-08 13:57:05 -0400

    Checkpoint featurized data; add iterative updates.

--- scripts/cascade-features.py
+++ scripts/cascade-features.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 
-import json
 import sys
 import numpy as np
 from numpy.linalg import inv
@@ -39,7 +38,7 @@ def padUpleft(m):
                            np.concatenate((np.zeros((1, size)), m), axis=0)),
                           axis=1)
 
-def featureGradients(c, w):
+def clusterGradients(c, w):
     n = max(map(lambda r: r.dst, c[1])) + 1
 
     numL = np.zeros((n, n))
@@ -64,16 +63,7 @@ def featureGradients(c, w):
 
     return fgrad
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: cascade-features.py <input> <output>", file=sys.stderr)
-        exit(-1)
-    sc = SparkContext(appName='Cascade Features')
-    sqlContext = SQLContext(sc)
-    
-    gap = 730
-
-    raw = sqlContext.read.load(sys.argv[1])
+def featurizeData(raw, gap, vocabFile, featFile):
     feats = raw.dropDuplicates(['cluster', 'series', 'date'])\
             .withColumn('day', datediff(col('date'), lit('1970-01-01')))\
             .na.drop(subset=['day'])\
@@ -84,21 +74,54 @@ if __name__ == "__main__":
     feats.cache()
     cv = CountVectorizer(inputCol='raw', outputCol='features', minDF=4.0)
     interner = cv.fit(feats)      # alternate possibility: grab features only from label==1 edges
-
-    fcount = len(interner.vocabulary)
-    w = np.zeros((fcount,))
-
-    json.dump(interner.vocabulary, open(sys.argv[2], 'w'))
-
     full = interner.transform(feats)
-    full.write.json(sys.argv[3])
+
+    full.write.parquet(featFile)
+    np.savetxt(vocabFile, np.array(interner.vocabulary), fmt='%s')
+    feats.unpersist()
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: cascade-features.py <input> <output>", file=sys.stderr)
+        exit(-1)
+    sc = SparkContext(appName='Cascade Features')
+    sqlContext = SQLContext(sc)
+    
+    gap = 730
+    odir = sys.argv[2]
+    vocabFile = odir + "/vocab.gz"
+    featFile = odir + "/feats.parquet"
+
+    try:
+        full = sqlContext.read.load(featFile)
+        vocab = np.loadtxt(vocabFile, 'string')
+    except:
+        featurizeData(sqlContext.read.load(sys.argv[1]), gap, vocabFile, featFile)
+        full = sqlContext.read.load(featFile)
+        vocab = np.loadtxt(vocabFile, 'string')
+
+    fcount = len(vocab)
+    w = np.zeros(fcount)
     
     fdata = full.select('cluster', 'src', 'dst', 'label', 'features')\
                 .rdd.groupBy(lambda r: r.cluster)
     fdata.cache()
 
-    grad = fdata.flatMap(lambda c: featureGradients(c, w))\
-                .toDF(['feat', 'grad']).groupBy('feat').agg(gsum('grad').alias('grad'))
-    grad.write.json(sys.argv[4])
+    iter = 20
+    rate = 1.0
+    var = 1.0
+
+    for i in range(iter):
+        grad = fdata.flatMap(lambda c: clusterGradients(c, w)).toDF(['feat', 'grad'])\
+                    .groupBy('feat').agg(gsum('grad').alias('grad'))\
+                    .collect()
+        update = np.zeros(fcount)
+        for g in grad:
+            update[g.feat] = g.grad
+        if var > 0:
+            update += w / var
+        w -= rate * update
+        print(update)
+        np.savetxt("%s/iter%03d.gz" % (odir, i), w)
     
     sc.stop()
