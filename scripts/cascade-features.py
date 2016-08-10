2c7d1d0e8dc9a9a42401d57e5fe8402fdfb8805e
David Smith <dasmiq@gmail.com>
2016-08-09 23:34:10 -0400

    Test posterior calculation.

--- scripts/cascade-features.py
+++ scripts/cascade-features.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-import sys
+import argparse
 import numpy as np
 from numpy.linalg import inv
 from math import log
@@ -23,7 +23,7 @@ def clusterFeatures(c, gap):
     for d in range(n):
         dst = wits[d]
         res.append(Row(cluster=long(c[0]), src=0, dst=d+1, label=1,
-                       raw=["root", "root:" + dst.series]))
+                       raw=["root:" + dst.series]))
         for s in range(n):
             src = wits[s]
             if (s != d) and (abs(dst.day - src.day) < gap):
@@ -37,6 +37,32 @@ def padUpleft(m):
     return np.concatenate((np.zeros((size+1, 1)),
                            np.concatenate((np.zeros((1, size)), m), axis=0)),
                           axis=1)
+
+## Should figure out how to reuse this in clusterGradients
+def clusterPosteriors(c, w):
+    n = max(map(lambda r: r.dst, c[1])) + 1
+
+    numL = np.zeros((n, n))
+    denL = np.zeros((n, n))
+    for r in c[1]:
+        score = -np.exp(w[np.array(r.features.indices)].dot(r.features.values))
+        numL[r.src, r.dst] = score if r.label == 1 else 0
+        denL[r.src, r.dst] = score
+    numL += np.diag(-numL.sum(axis=0))
+    denL += np.diag(-denL.sum(axis=0))
+
+    numLinv = padUpleft(inv(numL[1:,1:]))
+    denLinv = padUpleft(inv(denL[1:,1:]))
+
+    posts = []
+    for r in c[1]:
+        mom = r.src
+        kid = r.dst
+        post = -numL[mom, kid] * (numLinv[kid, mom] - numLinv[kid, kid]) + \
+               denL[mom, kid] * (denLinv[kid, mom] - denLinv[kid, kid])
+        posts.append(Row(lpost=float(post), **(r.asDict())))
+    return posts
+
 
 def clusterGradients(c, w):
     n = max(map(lambda r: r.dst, c[1])) + 1
@@ -81,24 +107,32 @@ def featurizeData(raw, gap, vocabFile, featFile):
     feats.unpersist()
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: cascade-features.py <input> <output>", file=sys.stderr)
-        exit(-1)
+    argparser = argparse.ArgumentParser(description='Cascade features')
+    argparser.add_argument('-i', '--input', nargs=1)
+    argparser.add_argument('-p', '--posteriors', nargs='?', metavar='params')
+    argparser.add_argument('outdir', help='Output directory')
+    args = argparser.parse_args()
+
     sc = SparkContext(appName='Cascade Features')
     sqlContext = SQLContext(sc)
     
     gap = 730
-    odir = sys.argv[2]
-    vocabFile = odir + "/vocab.gz"
-    featFile = odir + "/feats.parquet"
+    vocabFile = args.outdir + "/vocab.gz"
+    featFile = args.outdir + "/feats.parquet"
 
     try:
         full = sqlContext.read.load(featFile)
         vocab = np.loadtxt(vocabFile, 'string')
     except:
-        featurizeData(sqlContext.read.load(sys.argv[1]), gap, vocabFile, featFile)
+        featurizeData(sqlContext.read.load(args.input[0]), gap, vocabFile, featFile)
         full = sqlContext.read.load(featFile)
         vocab = np.loadtxt(vocabFile, 'string')
+
+    if args.posteriors:
+        w = np.loadtxt(args.posteriors)
+        full.rdd.groupBy(lambda r: r.cluster).flatMap(lambda c: clusterPosteriors(c, w)).toDF()\
+            .write.save(args.posteriors + ".parquet")
+        exit(0)
 
     fcount = len(vocab)
     w = np.zeros(fcount)
@@ -108,8 +142,8 @@ if __name__ == "__main__":
     fdata.cache()
 
     iter = 20
-    rate = 1.0
-    var = 1.0
+    rate = 1e-06                # scale with training size?
+    var = 0.1
 
     for i in range(iter):
         grad = fdata.flatMap(lambda c: clusterGradients(c, w)).toDF(['feat', 'grad'])\
@@ -122,6 +156,6 @@ if __name__ == "__main__":
             update += w / var
         w -= rate * update
         print(update)
-        np.savetxt("%s/iter%03d.gz" % (odir, i), w)
+        np.savetxt("%s/iter%03d.gz" % (args.outdir, i), w)
     
     sc.stop()
