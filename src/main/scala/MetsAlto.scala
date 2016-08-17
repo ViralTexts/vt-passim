e0d3b40a0238d9733d6e14acb7301a403c0ea704
David Smith <dasmiq@gmail.com>
2016-08-16 22:53:11 -0400

    Ensure unique IDs; better metadata calculation.

--- src/main/scala/MetsAlto.scala
+++ src/main/scala/MetsAlto.scala
@@ -5,11 +5,12 @@ import org.apache.spark.sql.SQLContext
 
 import collection.JavaConversions._
 import scala.collection.mutable.{ArrayBuffer, StringBuilder}
+import scala.util.Try
 
 import vtpassim.pageinfo._
 
-case class Record(id: String, issue: String, series: String, date: String, lang: String,
-  dpi: Int, text: String, regions: Array[Region])
+case class Record(id: String, issue: String, series: String, seq: Int,
+  date: String, lang: String, dpi: Int, text: String, regions: Array[Region])
 
 object MetsAlto {
   def main(args: Array[String]) {
@@ -28,42 +29,45 @@ object MetsAlto {
           val issue = fname.getName.replaceAll(".zip$", "")
 
           val zfile = new java.util.zip.ZipFile(fname)
-          val (series, date, lang, dpi) =
-            try {
-              val mfile = zfile.entries.filter(_.getName.endsWith("mets.xml")).toSeq.head
-              val t = scala.xml.XML.load(zfile.getInputStream(mfile))
-              ((t \\ "identifier").head.text,
-                (t \\ "dateIssued").head.text
-                  .replaceAll("""^(\d\d)\.(\d\d)\.(\d\d\d\d)$""", """$3-$2-$1"""),
-                (t \\ "languageTerm").head.text,
-                (t \\ "XphysScanResolution").head.text.toInt)
-            } catch {
-              case e: Exception => ("", "", "", 0)
-            }
+          val mfile = zfile.entries.filter(_.getName.endsWith("mets.xml")).toSeq.head
+          val t = scala.xml.XML.load(zfile.getInputStream(mfile))
+          val series = (t \\ "identifier").head.text
+          val lang = (t \\ "languageTerm").head.text
+          val dpi = Try((t \\ "XphysScanResolution").head.text.toInt).getOrElse(0)
 
-          zfile.entries.filter(f => f.getName.endsWith(".xml") && !f.getName.endsWith("mets.xml"))
-            .map(f => {
-              val t = scala.xml.XML.load(zfile.getInputStream(f))
-              val buf = new StringBuilder
-              val regions = new ArrayBuffer[Region]
+          val date = (t \\ "dateIssued").text
+            .replaceAll("""^(\d\d)\.(\d\d)\.(\d\d\d\d)$""", """$3-$2-$1""")
+
+          zfile.entries
+            .filter(f => f.getName.endsWith(".xml") && !f.getName.endsWith("mets.xml"))
+            .map { f =>
+            val t = scala.xml.XML.load(zfile.getInputStream(f))
+            val buf = new StringBuilder
+            val regions = new ArrayBuffer[Region]
+            val seq = Try((t \\ "Page" \ "@PHYSICAL_IMG_NR").text.toInt).getOrElse(0)
               (t \\ "TextLine").foreach { line =>
-                (line \ "_").foreach({ e =>
+                (line \ "_").foreach { e =>
                   if ( e.label == "String" ) {
                     val start = buf.size
                     buf.append((e \ "@CONTENT").text.replaceAll("&", "&amp;").replaceAll("<", "&lt;"))
-                    regions += Region(start, buf.size - start,
-                      Coords((e \ "@HPOS").text.toInt, (e \ "@VPOS").text.toInt,
-                        (e \ "@WIDTH").text.toInt, (e \ "@HEIGHT").text.toInt, (e \ "@HEIGHT").text.toInt))
+                    try {
+                      regions += Region(start, buf.size - start,
+                        Coords((e \ "@HPOS").text.toInt, (e \ "@VPOS").text.toInt,
+                          (e \ "@WIDTH").text.toInt, (e \ "@HEIGHT").text.toInt, (e \ "@HEIGHT").text.toInt))
+                    } catch {
+                      case e: Exception =>
+                    }
                   } else if ( e.label == "SP" ) {
                     buf.append(" ")
                   } else if ( e.label == "HYP" ) {
                     buf.append("\u00ad")
                   }
-                })
+                }
                 buf.append("\n")
               }
-              Record(f.getName.replaceAll(".xml$", ""), issue, series, date, lang, dpi, buf.toString, regions.toArray)
-            })
+            Record(issue + "/" + f.getName.replaceAll(".xml$", ""), issue, series, seq,
+              date, lang, dpi, buf.toString, regions.toArray)
+          }
         } catch {
           case e: Exception => {
             println(x._1 + e.toString)
