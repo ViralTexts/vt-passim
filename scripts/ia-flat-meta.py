62fe45c5142d39eb2804ffab087f22f70aeb29ae
David Smith <dasmiq@gmail.com>
2017-01-04 10:27:12 -0500

    Flatten repeated fields in IA metadata.

---
+++ scripts/ia-flat-meta.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+import sys
+from pyspark.sql import SparkSession
+
+def flattenRow(r):
+    res = dict()
+    for elem in r['str']:
+        res[elem['_name']] = elem['_VALUE']
+    for elem in r['arr']:
+        res[elem['_name']] = '; '.join(elem['str'])
+    return res
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: %s <input> <output>" % sys.argv[0], file=sys.stderr)
+        exit(-1)
+    spark = SparkSession.builder.appName('IA Metadata').getOrCreate()
+    raw = spark.read.format('com.databricks.spark.xml').option('rowTag', 'doc').load(sys.argv[1])
+
+    spark.createDataFrame(raw.rdd.map(flattenRow), samplingRatio=1).write.save(sys.argv[2])
+    spark.stop()
+    
+    
