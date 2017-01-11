39c04f2b052e2e07f8752a05095090d76847305b
David Smith <dasmiq@gmail.com>
2017-01-11 10:56:41 -0500

    Use SparkSession interface.

--- scripts/c19.py
+++ scripts/c19.py
@@ -2,17 +2,15 @@ from __future__ import print_function
 
 import sys
 
-from pyspark import SparkContext
-from pyspark.sql import SQLContext
+from pyspark.sql import SparkSession
 from pyspark.sql.functions import lit, col
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: c19.py <input> <output>", file=sys.stderr)
         exit(-1)
-    sc = SparkContext(appName="SelectC19")
-    sqlContext = SQLContext(sc)
-    raw = sqlContext.read.option('mergeSchema','true').load(sys.argv[1])
+    spark = SparkSession.builder.appName('Select c19').getOrCreate()
+    raw = spark.read.option('mergeSchema','true').load(sys.argv[1])
     df = raw.filter(col('date') < '1900')
 
     opens = df.filter(col('open') == 'true')\
@@ -24,4 +22,4 @@ if __name__ == "__main__":
       .dropDuplicates(['id'])\
       .write.save(sys.argv[2])
 
-    sc.stop()
+    spark.stop()
