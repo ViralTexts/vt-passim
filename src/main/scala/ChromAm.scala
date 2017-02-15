b21888710213627dd3c41b9814f44e5d2b047f92
David Smith <dasmiq@gmail.com>
2017-02-15 13:27:11 -0500

    Restrict page width and height expressions.

--- src/main/scala/ChromAm.scala
+++ src/main/scala/ChromAm.scala
@@ -41,8 +41,8 @@ object ChronAm {
         val sb = new StringBuilder
         val regions = new ArrayBuffer[Region]
 
-        val width = Try((t \\ "Page" \ "@WIDTH").text.toInt).getOrElse(0)
-        val height = Try((t \\ "Page" \ "@HEIGHT").text.toInt).getOrElse(0)
+        val width = Try((t \ "Layout" \ "Page" \ "@WIDTH").text.toInt).getOrElse(0)
+        val height = Try((t \ "Layout" \ "Page" \ "@HEIGHT").text.toInt).getOrElse(0)
 
           (t \\ "TextBlock") foreach { block =>
             (block \ "TextLine" ) foreach { line =>
