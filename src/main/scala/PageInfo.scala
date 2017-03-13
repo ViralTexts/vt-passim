705fc03e19a1744f09408de9a8cbb6a1ee6f3ebd
David Smith <dasmiq@gmail.com>
2017-03-13 10:17:09 -0400

    Add Locus definition for cited passages.

--- src/main/scala/PageInfo.scala
+++ src/main/scala/PageInfo.scala
@@ -4,4 +4,6 @@ case class Coords(x: Int, y: Int, w: Int, h: Int, b: Int)
 
 case class Region(start: Int, length: Int, coords: Coords)
 
+case class Locus(start: Int, length: Int, loc: String)
+
 case class Page(id: String, seq: Int, width: Int, height: Int, dpi: Int, regions: Array[Region])
