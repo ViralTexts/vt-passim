d9f3a7bc0d548815482f1c6542f3fef63e55ca89
David Smith <dasmiq@gmail.com>
2017-05-30 14:47:42 -0400

    Use SparkSession.

--- scripts/trove-load.py
+++ scripts/trove-load.py
@@ -1,19 +1,18 @@
 from __future__ import print_function
 
 import sys
+import textwrap
 
-from pyspark import SparkContext
-from pyspark.sql import SQLContext
+from pyspark.sql import SparkSession
 from pyspark.sql.functions import lit, concat, concat_ws, regexp_replace
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: trove-load.py <input json> <output parquet>", file=sys.stderr)
         exit(-1)
-    sc = SparkContext(appName="Trove Load")
-    sqlContext = SQLContext(sc)
+    spark = SparkSession.builder.appName('Trove Load').getOrCreate()
 
-    raw = sqlContext.read.json(sys.argv[1])
+    raw = spark.read.json(sys.argv[1])
     df = raw.na.drop(subset=['id', 'fulltext']).dropDuplicates(['id'])
     df.select(concat(lit('trove/'), df.id).alias('id'),
               concat_ws('/', lit('trove'), df.titleId, df.date).alias('issue'),
@@ -24,4 +23,4 @@ if __name__ == "__main__":
                              '<', '&lt;').alias('text'))\
       .write.save(sys.argv[2])
 
-    sc.stop()
+    spark.stop()
