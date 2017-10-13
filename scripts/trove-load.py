26d564a64d93730b4fc9595a5d0b68da826cb566
David Smith <dasmiq@gmail.com>
2017-10-13 08:46:39 -0400

    Wrap Trove text to create pseudo-lines.

--- scripts/trove-load.py
+++ scripts/trove-load.py
@@ -4,13 +4,17 @@ import sys
 import textwrap
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import lit, concat, concat_ws, regexp_replace
+from pyspark.sql.functions import lit, concat, concat_ws, regexp_replace, udf
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: trove-load.py <input json> <output parquet>", file=sys.stderr)
         exit(-1)
     spark = SparkSession.builder.appName('Trove Load').getOrCreate()
+
+    wrapper = textwrap.TextWrapper(width=40, break_long_words=False)
+
+    text_fill = udf(lambda s: wrapper.fill(s))
 
     raw = spark.read.json(sys.argv[1])
     df = raw.na.drop(subset=['id', 'fulltext']).dropDuplicates(['id'])
@@ -19,8 +23,8 @@ if __name__ == "__main__":
               concat(lit('trove/'), df.titleId).alias('series'),
               df.date, df.firstPageId, df.firstPageSeq.cast('int').alias('seq'),
               df.heading.alias('title'), df.category,
-              regexp_replace(regexp_replace(df.fulltext, '&', '&amp;'),
-                             '<', '&lt;').alias('text'))\
+              text_fill(regexp_replace(regexp_replace(df.fulltext, '&', '&amp;'),
+                             '<', '&lt;')).alias('text'))\
       .write.save(sys.argv[2])
 
     spark.stop()
