2223586687e451496b87847bdf5bb8955d7abe3c
David Smith <dasmiq@gmail.com>
2018-09-26 14:33:38 -0400

    First draft of cts-load.py

---
+++ scripts/cts-load.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+from re import sub
+import sys
+from os.path import basename, splitext
+from pyspark.sql import SparkSession, Row
+
+def parseCTS(f):
+    res = dict()
+    text = ''
+    locs = []
+    for line in f[1].split('\n'):
+        if line != '':
+            (loc, raw) = line.split('\t', 2)
+            start = len(text)
+            text += sub('\s+', ' ', raw)
+            text += '\n'
+            locs.append(Row(start=start, length=(len(text) - start), loc=loc))
+
+    return Row(id=(splitext(basename(f[0])))[0], text=text, locs=locs)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: cts-load.py <input> <output>", file=sys.stderr)
+        exit(-1)
+    spark = SparkSession.builder.appName('Load CTS TSV').getOrCreate()
+    spark.sparkContext.setLogLevel('WARN')
+
+    spark.sparkContext.wholeTextFiles(sys.argv[1]) \
+        .filter(lambda f: f[0].endswith('.cts')) \
+        .map(lambda f: parseCTS(f)) \
+        .toDF() \
+        .write.save(sys.argv[2])
+    spark.stop()
+    
