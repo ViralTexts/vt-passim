f890c7d56e57acee65924ca37c762b453a8c00ff
David Smith <dasmiq@gmail.com>
2018-11-01 22:03:19 -0400

    Refactor common code into MetsAlto.scala.

---
+++ src/main/scala/MetsAlto.scala
@@ -0,0 +1,32 @@
+package vtpassim
+
+import scala.collection.mutable.{ArrayBuffer, StringBuilder}
+import vtpassim.pageinfo._
+
+object MetsAlto {
+  def altoText(t: scala.xml.NodeSeq) = {
+    val buf = new StringBuilder
+    val regions = new ArrayBuffer[Region]
+      (t \\ "TextLine").foreach { line =>
+        (line \ "_").foreach { e =>
+          if ( e.label == "String" ) {
+            val start = buf.size
+            buf.append((e \ "@CONTENT").text.replaceAll("&", "&amp;").replaceAll("<", "&lt;"))
+            try {
+              regions += Region(start, buf.size - start,
+                Coords((e \ "@HPOS").text.toInt, (e \ "@VPOS").text.toInt,
+                  (e \ "@WIDTH").text.toInt, (e \ "@HEIGHT").text.toInt, (e \ "@HEIGHT").text.toInt))
+            } catch {
+              case e: Exception =>
+            }
+          } else if ( e.label == "SP" ) {
+            buf.append(" ")
+          } else if ( e.label == "HYP" ) {
+            buf.append("\u00ad")
+          }
+        }
+        buf.append("\n")
+      }
+    (buf.toString, regions.toArray)
+  }
+}
--- src/main/scala/MetsAltoZipped.scala
+++ src/main/scala/MetsAltoZipped.scala
@@ -1,11 +1,8 @@
 package vtpassim
 
 import org.apache.spark.sql.SparkSession
-
 import collection.JavaConversions._
-import scala.collection.mutable.{ArrayBuffer, StringBuilder}
 import scala.util.Try
-
 import vtpassim.pageinfo._
 
 case class Record(id: String, issue: String, series: String, seq: Int,
@@ -41,35 +38,14 @@ object MetsAltoZipped {
             .filter(f => f.getName.endsWith(".xml") && !f.getName.endsWith("mets.xml"))
             .map { f =>
             val t = scala.xml.XML.load(zfile.getInputStream(f))
-            val buf = new StringBuilder
-            val regions = new ArrayBuffer[Region]
             val page = (t \ "Layout" \ "Page")
             val seq = Try((page \ "@PHYSICAL_IMG_NR").text.toInt).getOrElse(0)
             val width = Try((page \ "@WIDTH").text.toInt).getOrElse(0)
             val height = Try((page \ "@HEIGHT").text.toInt).getOrElse(0)
-              (t \\ "TextLine").foreach { line =>
-                (line \ "_").foreach { e =>
-                  if ( e.label == "String" ) {
-                    val start = buf.size
-                    buf.append((e \ "@CONTENT").text.replaceAll("&", "&amp;").replaceAll("<", "&lt;"))
-                    try {
-                      regions += Region(start, buf.size - start,
-                        Coords((e \ "@HPOS").text.toInt, (e \ "@VPOS").text.toInt,
-                          (e \ "@WIDTH").text.toInt, (e \ "@HEIGHT").text.toInt, (e \ "@HEIGHT").text.toInt))
-                    } catch {
-                      case e: Exception =>
-                    }
-                  } else if ( e.label == "SP" ) {
-                    buf.append(" ")
-                  } else if ( e.label == "HYP" ) {
-                    buf.append("\u00ad")
-                  }
-                }
-                buf.append("\n")
-              }
+            val (text, regions) = MetsAlto.altoText(t)
             Record(issue + "/" + f.getName.replaceAll(".xml$", ""), issue, series, seq,
-              date, lang, buf.toString,
-              Array(Page((page \ "@ID").text, seq, width, height, dpi, regions.toArray)))
+              date, lang, text,
+              Array(Page((page \ "@ID").text, seq, width, height, dpi, regions)))
           }
         } catch {
           case e: Exception => {
