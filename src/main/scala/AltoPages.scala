50f6c76e7dcc892131d6fc1be7452f9988265c10
David Smith <dasmiq@gmail.com>
2019-08-09 09:49:34 -0400

    Extract sequence of pages from plain Alto.

---
+++ src/main/scala/AltoPages.scala
@@ -0,0 +1,35 @@
+package vtpassim
+
+import org.apache.spark.sql.SparkSession
+import collection.JavaConversions._
+import scala.util.Try
+import vtpassim.pageinfo._
+
+case class AltoPage(id: String, series: String, seq: Int, text: String, pages: Array[Page])
+
+object AltoPages {
+  def main(args: Array[String]) {
+    val spark = SparkSession.builder().appName("MetsAlto import").getOrCreate()
+    import spark.implicits._
+
+    spark.sparkContext.hadoopConfiguration
+      .set("mapreduce.input.fileinputformat.input.dir.recursive", "true")
+
+    spark.sparkContext.binaryFiles(args(0), spark.sparkContext.defaultParallelism)
+      .filter(_._1.endsWith(".xml"))
+      .flatMap( in => {
+        val t = scala.xml.XML.load(in._1)
+          (t \\ "Page").map { page =>
+            val id = (page \ "@ID").text
+            val seq = (page \ "@PHYSICAL_IMG_NR").text.toInt
+            val (text, regions) = MetsAlto.altoText(page)
+            AltoPage(id, in._1, seq, text,
+              Array(Page(id, seq,
+                (page \ "@WIDTH").text.toInt, (page \ "@HEIGHT").text.toInt, 0, regions)))
+          }
+      })
+      .toDF
+      .write.json(args(1))
+    spark.stop()
+  }
+}
