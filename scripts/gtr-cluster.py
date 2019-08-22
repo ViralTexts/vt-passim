b031b5e4c87f62711fef655e79807feb5df0dfa1
David Smith <dasmiq@gmail.com>
2019-08-22 12:18:02 -0400

    Make list of page images.

--- scripts/gtr-cluster.py
+++ scripts/gtr-cluster.py
@@ -20,26 +20,34 @@ def formatPassage(r):
     if r.placeOfPublication:
         dateline += ' &middot; %s' % r.placeOfPublication
     text += "<h4>%s</h4>" % dateline
-    if r.open == 'true':
+    if r.ref > 0 or r.open == 'true':
         text += '<table style="width: 100%;"><tr><td style="width: 50%">' + sub('(?<!\\\\)\\n', '<br/>\\n', r.text) + '</td>'
         if r.page_image:
-            text += f'<td style="width: 50%; max-height: 75%; margin: auto; display: block;"><img src="{r.page_image}"></td>'
+            text += f'<td style="width: 50%; max-height: 75%; margin: auto; display: block;"><img></td>'
         text += '</tr></table>'
     else:
         text += '[This text is not available under open license.]'
     
-    return Row(cluster=r.cluster, ref=r.ref, date=r.date, id=r.id, begin=r.begin, text=text)
+    return Row(cluster=r.cluster, ref=r.ref, date=r.date, id=r.id, begin=r.begin, text=text, page_image=r.page_image)
     
 def formatCluster(x):
     (cluster, riter) = x
     rows = list(riter)
+    images = list()
     rows.sort(key=lambda z: (-z.ref, z.date, z.id, z.begin))
     name = rows[0].id
     title = "%d reprints from %s to %s [cl%d]" % (len(rows), rows[0].date, rows[len(rows)-1].date, cluster)
-    text = f"<h1>{title}</h1>\n"
+    text = f'<html><head><title>{title}</title></head>\n<body>'
+    text += f'<h1>{title}</h1>\n'
     for i in range(len(rows)):
-        text += "<div n=\"%d\">%s</div><hr />\n" % (i, rows[i].text)
-    return Row(name=name, text=text)
+        cur = rows[i].text
+        if rows[i].page_image:
+            imgid = f'{name}-{i}'
+            cur = sub(r'<img>', f'<img id="{imgid}" src="{imgid}.jpg">', cur)
+            images.append(Row(id=imgid, src=rows[i].page_image))
+        text += "<div n=\"%d\">%s</div><hr />\n" % (i, cur)
+    text += '</body></html>'
+    return Row(name=name, text=text, images=images)
 
 
 if __name__ == "__main__":
