5a83ca5ead0eeff7b53a93d5aad4d0098aa0abc8
David Smith <dasmiq@gmail.com>
2019-09-09 16:18:55 -0400

    Initial copy of ChronAm.scala.

---
+++ src/main/scala/ImpressoChronAm.scala
@@ -0,0 +1,98 @@
+package vtpassim
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{array, struct}
+import org.apache.hadoop.io.Text
+
+import collection.JavaConversions._
+import scala.collection.mutable.{ArrayBuffer, StringBuilder}
+import scala.util.Try
+
+import vtpassim.pageinfo._
+
+case class RawCARecord(id: String, caid: String,
+  issue: String, series: String, ed: String, seq: Int,
+  batch: String, date: String, text: String,
+  width: Int, height: Int, dpi: Int, regions: Array[Region])
+
+object ImpressoChronAm {
+  def cleanInt(s: String): Int = s.replaceFirst("\\.0*$", "").toInt
+  def main(args: Array[String]) {
+    val spark = SparkSession.builder().appName("ChronAm Import").getOrCreate()
+    import spark.implicits._
+
+    spark.sparkContext.hadoopConfiguration
+      .set("mapreduce.input.fileinputformat.input.dir.recursive", "true")
+
+    val files = spark.read.json(args(1))
+
+    val records =
+      spark.sparkContext.newAPIHadoopFile(args(0), classOf[TarballInputFormat],
+        classOf[TarballEntry], classOf[Text])
+        .filter { _._1.getEntry.endsWith(".xml") }
+        .flatMap { raw =>
+        val fname = raw._1.getEntry
+        try {
+          val batch = raw._1.getTarball.replaceAll("\\.tar\\.bz2$", "")
+          val contents = raw._2.toString
+          val clean = if ( contents.startsWith("\ufeff") ) contents.substring(1) else contents
+          val t = scala.xml.XML.loadString(clean)
+          val Array(sn, year, month, day, ed, seq, _*) = fname.split("/")
+          val series = s"/lccn/$sn"
+          val date = s"$year-$month-$day"
+          val issue = Seq("/ca", batch, sn, date, ed) mkString "/"
+          val caid = s"$issue/$seq"
+
+          val sb = new StringBuilder
+          val regions = new ArrayBuffer[Region]
+
+          val width = Try((t \ "Layout" \ "Page" \ "@WIDTH").text.toInt).getOrElse(0)
+          val height = Try((t \ "Layout" \ "Page" \ "@HEIGHT").text.toInt).getOrElse(0)
+
+          val dpi = Try(1200 / "xdpi:([0-9]+)".r.findFirstMatchIn((t \ "Description" \\ "processingStepSettings").text).get.group(1).toInt).getOrElse(0)
+
+          (t \\ "TextBlock") foreach { block =>
+            (block \ "TextLine" ) foreach { line =>
+              (line \ "_") foreach { e =>
+                e.label match {
+                  case "String" =>
+                    val start = sb.size
+                    sb ++= (e \ "@CONTENT").text
+                    try {
+                      regions += Region(start, sb.size - start,
+                        Coords(cleanInt(e \\ "@HPOS" text), cleanInt(e \\ "@VPOS" text),
+                          cleanInt(e \\ "@WIDTH" text), cleanInt(e \\ "@HEIGHT" text),
+                          cleanInt(e \\ "@HEIGHT" text)))
+                    } catch {
+                      case ex: Exception =>
+                    }
+                  case "SP" => sb ++= " "
+                  case "HYP" => sb ++= "\u00ad"
+                  case _ =>
+                }
+              }
+              sb ++= "\n"
+            }
+            sb ++= "\n"
+          }
+
+        val nseq = Try(seq.replace("seq-", "").toInt).getOrElse(0)
+
+        Some(CARecord(caid, issue, series, ed.replace("ed-", ""), nseq,
+          batch, date, sb.toString, width, height, dpi, regions.toArray))
+      } catch {
+        case ex: Exception =>
+          Console.err.println("## " + fname + ": " + ex.toString)
+          None
+      }
+    }
+      .toDF
+      .dropDuplicates("id") // NB: id now includes batch
+      .join(files.select("id", "file"), "id")
+      .withColumn("pages", array(struct('file as "id", 'seq, 'width, 'height, 'dpi, 'regions)))
+      .drop("file", "width", "height", "dpi", "regions")
+      .write.save(args(2))
+    spark.stop()
+  }
+}
+
