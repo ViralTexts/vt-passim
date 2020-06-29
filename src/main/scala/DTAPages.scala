35486dc4c93ed8f3e9504078e718c20745a60712
David Smith <dasmiq@gmail.com>
2020-06-29 14:28:47 -0400

    Add ztype field for zone type information.

--- src/main/scala/DTAPages.scala
+++ src/main/scala/DTAPages.scala
@@ -8,10 +8,12 @@ import scala.xml.pull._
 
 case class RenditionSpan(rendition: String, start: Int, length: Int)
 
-case class ZoneContent(place: String, data: StringBuilder, rend: ListBuffer[RenditionSpan])
+case class ZoneInfo(ztype: String, place: String)
 
-case class Rec(id: String, book: String, seq: Int, page: String, place: String, text:String,
-  rendition: Array[RenditionSpan])
+case class ZoneContent(info: ZoneInfo, data: StringBuilder, rend: ListBuffer[RenditionSpan])
+
+case class Rec(id: String, book: String, seq: Int, page: String, ztype: String, place: String,
+  text:String, rendition: Array[RenditionSpan])
 
 object DTAPages {
   def main(args: Array[String]) {
@@ -68,43 +70,43 @@ object DTAPages {
             case EvElemStart(_, "pb", attr, _) => {
               val res = new ListBuffer[Rec]()
               // Remember and output all open zones
-              val places = (if ( zoneStack.isEmpty ) Seq("body") else zoneStack.toSeq.map(_.place).reverse)
+              val zones = (if ( zoneStack.isEmpty ) Seq(ZoneInfo("body", "body")) else zoneStack.toSeq.map(_.info).reverse)
               while ( !zoneStack.isEmpty ) {
                 seq += 1
                 val top = zoneStack.pop
-                res += Rec(pageID + s"z$seq", book, seq, pageID, top.place, top.data.toString, top.rend.toArray)
+                res += Rec(pageID + s"z$seq", book, seq, pageID, top.info.ztype, top.info.place, top.data.toString, top.rend.toArray)
               }
-              for ( place <- places ) {
-                zoneStack.push(new ZoneContent(place, new StringBuilder, new ListBuffer[RenditionSpan]()))
+              for ( zone <- zones ) {
+                zoneStack.push(new ZoneContent(zone, new StringBuilder, new ListBuffer[RenditionSpan]()))
               }
               // Output printed page number (n attribute not in brackets) here?
               pageID = book + attr("facs").text
               val pno = Try(attr("n").text).getOrElse("").replaceAll("\\[[^\\]]+\\]", "")
               if ( pno != "" ) {
                 seq += 1
-                res += Rec(pageID + s"z$seq", book, seq, pageID, "page", pno, new Array[RenditionSpan](0))
+                res += Rec(pageID + s"z$seq", book, seq, pageID, "pageNum", "pageNum", pno, new Array[RenditionSpan](0))
               }
               res
             }
             case EvElemStart(_, "cb", attr, _) => {
               val res = new ListBuffer[Rec]()
               // Remember and output all open zones
-              val places = (if ( zoneStack.isEmpty ) Seq("body") else zoneStack.toSeq.map(_.place).reverse)
+              val zones = (if ( zoneStack.isEmpty ) Seq(ZoneInfo("body", "body")) else zoneStack.toSeq.map(_.info).reverse)
               while ( !zoneStack.isEmpty ) {
                 val top = zoneStack.pop
                 if ( top.data.toString != "" ) {
                   seq += 1
-                  res += Rec(pageID + s"z$seq", book, seq, pageID, top.place, top.data.toString, top.rend.toArray)
+                  res += Rec(pageID + s"z$seq", book, seq, pageID, top.info.ztype, top.info.place, top.data.toString, top.rend.toArray)
                 }
               }
-              for ( place <- places ) {
-                zoneStack.push(new ZoneContent(place, new StringBuilder, new ListBuffer[RenditionSpan]()))
+              for ( zone <- zones ) {
+                zoneStack.push(new ZoneContent(zone, new StringBuilder, new ListBuffer[RenditionSpan]()))
               }
               // Output printed column number (n attribute not in brackets) here?
               val cno = Try(attr("n").text).getOrElse("").replaceAll("\\[[^\\]]+\\]", "")
               if ( cno != "" ) {
                 seq += 1
-                res += Rec(pageID + s"z$seq", book, seq, pageID, "column", cno, new Array[RenditionSpan](0))
+                res += Rec(pageID + s"z$seq", book, seq, pageID, "colNum", "colNum", cno, new Array[RenditionSpan](0))
               }
               res
             }
@@ -113,13 +115,14 @@ object DTAPages {
               while ( !zoneStack.isEmpty ) {
                 seq += 1
                 val top = zoneStack.pop
-                res += Rec(pageID + s"z$seq", book, seq, pageID, top.place, top.data.toString, top.rend.toArray)
+                res += Rec(pageID + s"z$seq", book, seq, pageID, top.info.ztype, top.info.place, top.data.toString, top.rend.toArray)
               }
               res
             }
             case EvElemStart(_, "note", attr, _) => {
               if ( !zoneStack.isEmpty ) {
-                zoneStack.push(new ZoneContent(Try(attr("place").text).getOrElse("note"),
+                zoneStack.push(new ZoneContent(ZoneInfo("note",
+                  Try(attr("place").text).getOrElse("note")),
                   new StringBuilder, new ListBuffer[RenditionSpan]()))
               }
               Nil
@@ -128,13 +131,14 @@ object DTAPages {
               if ( !zoneStack.isEmpty ) {
                 val top = zoneStack.pop
                 seq += 1
-                Seq(Rec(pageID + s"z$seq", book, seq, pageID, top.place, top.data.toString, top.rend.toArray))
+                Seq(Rec(pageID + s"z$seq", book, seq, pageID, top.info.ztype, top.info.place, top.data.toString, top.rend.toArray))
               } else
                 Nil
             }
             case EvElemStart(_, "fw", attr, _) => {
               if ( !zoneStack.isEmpty ) {
-                zoneStack.push(new ZoneContent(Try(attr("place").text).getOrElse("fw"),
+                zoneStack.push(new ZoneContent(ZoneInfo(Try(attr("type").text).getOrElse("fw"),
+                  Try(attr("place").text).getOrElse("fw")),
                   new StringBuilder, new ListBuffer[RenditionSpan]()))
               }
               Nil
@@ -143,7 +147,7 @@ object DTAPages {
               if ( !zoneStack.isEmpty ) {
                 val top = zoneStack.pop
                 seq += 1
-                Seq(Rec(pageID + s"z$seq", book, seq, pageID, top.place, top.data.toString, top.rend.toArray))
+                Seq(Rec(pageID + s"z$seq", book, seq, pageID, top.info.ztype, top.info.place, top.data.toString, top.rend.toArray))
               } else
                 Nil
             }
