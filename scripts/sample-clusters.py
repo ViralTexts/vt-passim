583993b4208799c0f0aab3d30ce1e078652842d9
David Smith <dasmiq@gmail.com>
2021-03-09 23:45:43 -0500

    Count total qualifying clusters.

--- scripts/sample-clusters.py
+++ scripts/sample-clusters.py
@@ -13,9 +13,13 @@ def main(config):
 
     raw = spark.read.load(config.inputPath)
 
-    samples = raw.filter(config.filter).groupBy('cluster').agg(
-        f.min(struct('date', 'uid'))['uid'].alias('uid')).sample(fraction=0.01
-                                                                 ).limit(config.samples)
+    pop = raw.filter(config.filter).groupBy('cluster').agg(
+        f.min(struct('date', 'uid'))['uid'].alias('uid'))
+
+    total = pop.count()
+
+    samples = pop.sample(fraction=(config.samples / total)).limit(config.samples)
+    
     raw.join(samples, ['cluster', 'uid'], 'left_semi'
             ).repartition(1).sort('date', 'begin'
             ).coalesce(1).write.csv(config.outputPath,
@@ -28,9 +32,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Sample Clusters',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-f', '--filter', type=str, default='size >= 10 AND pboiler < 0.2',
-                        help='SQL query for reprints; default=size >= 10 AND pboiler < 0.2')
+                        help='SQL query for reprints')
     parser.add_argument('-s', '--samples', type=int, default=1000,
-                        help='Number of samples; default=1000')
+                        help='Number of samples')
     parser.add_argument('inputPath', metavar='<path>', help='input path')
     parser.add_argument('outputPath', metavar='<path>', help='output path')
 
