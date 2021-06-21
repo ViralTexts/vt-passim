002b485c347c149f9b92a051a73f03c014a1718e
David Smith <dasmiq@gmail.com>
2021-06-21 09:16:40 -0400

    Include cluster ID in output; check for missing fields.

--- scripts/gtr-cluster.py
+++ scripts/gtr-cluster.py
@@ -1,36 +1,39 @@
-from __future__ import print_function
-
-import sys
+import argparse
 from re import sub
 import html
 
 from pyspark.sql import SparkSession, Row
 from pyspark.sql.functions import col, udf, regexp_replace
+import pyspark.sql.functions as f
 
 def formatPassage(r):
     text = ""
     source = 'From ' + (('<cite>%s</cite>' % r.source) or 'unknown source')
     title = r.title or source
+    if 'ref' in r and r.ref > 0:
+        ref = 1
+    else:
+        ref = 0
     if r.url:
         text += "<h2><a href=\"%s\">%s</a></h2>" % (r.url, title)
     else:
         text += "<h2>%s</h2>" % title
-    if r.creator: text += '<h4>by %s</h4>' % r.creator
+    if ('creator' in r) and r.creator: text += '<h4>by %s</h4>' % r.creator
     if title != source: text += '<h4>%s</h4>' % source
     dateline = '<datetime>%s</datetime>' % r.date
     if r.placeOfPublication:
         dateline += ' &middot; %s' % r.placeOfPublication
     text += "<h4>%s</h4>" % dateline
-    if r.ref > 0 or r.open == 'true':
+    if ref > 0 or r.open == 'true':
         text += '<table style="width: 100%;"><tr><td style="width: 50%">' + sub('(?<!\\\\)\\n', '<br/>\\n', html.escape(r.text)) + '</td>'
         if r.page_image:
             scaled = r.page_image.replace('/full/', '/!600,600/')
             text += f'<td style="width: 50%; max-height: 75%; margin: auto; display: block;"><img src="{scaled}" /></td>'
         text += '</tr></table>'
     else:
-        text += '[This text is not available under open license.]'
+        text += '[This text is not available under an open license.]'
     
-    return Row(cluster=r.cluster, ref=r.ref, date=r.date, id=r.id, begin=r.begin, text=text)
+    return Row(cluster=r.cluster, ref=ref, date=r.date, id=r.id, begin=r.begin, text=text)
     
 def formatCluster(x):
     (cluster, riter) = x
@@ -44,27 +47,28 @@ def formatCluster(x):
         cur = rows[i].text
         text += "<div n=\"%d\">%s</div><hr />\n" % (i, cur)
     text += '</body></html>'
-    return Row(name=name, text=text)
-
+    return Row(cluster=cluster, name=name, text=text)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Usage: gtr-cluster.py <input> <cluster-out>", file=sys.stderr)
-        exit(-1)
-    spark = SparkSession.builder.appName('GtR Formatting').getOrCreate()
+    parser = argparse.ArgumentParser(description='GtR Data Dump',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-f', '--filter', type=str, default='size >= 10 AND pboiler < 0.2',
+                        help='SQL query for reprints')
+    parser.add_argument('prettyPath', metavar='<path>', help='pretty cluster path')
+    parser.add_argument('outputPath', metavar='<path>', help='output path')
 
-    df = spark.read.load(sys.argv[1])
-    
-    df \
-        .drop("locs")\
-        .drop("pages")\
-        .drop("regions")\
-        .withColumn('page_access', col('url')) \
-        .rdd \
-        .map(formatPassage)\
-        .groupBy(lambda r: r.cluster) \
-        .map(formatCluster) \
-        .toDF().write.json(sys.argv[2])
+    config = parser.parse_args()
+
+    spark = SparkSession.builder.appName('GtR Data Dump').getOrCreate()
+
+    spark.read.load(config.prettyPath
+        ).filter(config.filter
+        ).drop('locs', 'pages', 'regions'
+        ).withColumn('page_access', col('url')
+        ).rdd.map(formatPassage
+        ).groupBy(lambda r: r.cluster
+        ).map(formatCluster
+        ).toDF().write.json(config.outputPath)
     
     spark.stop()
     
