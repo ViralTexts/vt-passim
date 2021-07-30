6b1f9be6e848aa69ae3af0413b6d929d53afc978
David Smith <dasmiq@gmail.com>
2021-07-30 12:00:30 -0400

    Reintroduce deduping by series-date; optimize with broadcast.

--- scripts/c19.py
+++ scripts/c19.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import sys
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, struct, max
+from pyspark.sql.functions import broadcast, col, struct, max, min
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -11,7 +11,15 @@ if __name__ == "__main__":
         exit(-1)
     spark = SparkSession.builder.appName('Select c19').getOrCreate()
     raw = spark.read.option('mergeSchema','true').load(sys.argv[1])
-    raw.filter(col('date') < '1900'
-               ).write.partitionBy('open', 'corpus').save(sys.argv[2])
+    df = raw.filter(col('date') < '1900')
+
+    spark.conf.set('spark.sql.adaptive.enabled', 'true')
+
+    issues = df.groupBy('series', 'date'
+                ).agg(min(struct(~col('open').cast('boolean'),
+                                 col('corpus')))['corpus'].alias('corpus'))
+
+    df.join(broadcast(issues), ['series', 'date', 'corpus'], 'left_semi'
+        ).write.partitionBy('open', 'corpus').save(sys.argv[2])
 
     spark.stop()
