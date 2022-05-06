3b51c17cdd9ca3fa9bff54e3166d81175f9a45d5
David Smith <dasmiq@gmail.com>
2022-05-06 09:11:59 -0400

    Remove code to join image filenames.

--- src/main/scala/ChromAm.scala
+++ src/main/scala/ChromAm.scala
@@ -23,8 +23,6 @@ object ChronAm {
     spark.sparkContext.hadoopConfiguration
       .set("mapreduce.input.fileinputformat.input.dir.recursive", "true")
 
-    val files = spark.read.json(args(1))
-
     spark.sparkContext.newAPIHadoopFile(args(0), classOf[TarballInputFormat],
       classOf[TarballEntry], classOf[Text])
       .filter { _._1.getEntry.endsWith(".xml") }
@@ -85,11 +83,7 @@ object ChronAm {
       }
     }
       .toDF
-      .dropDuplicates("series", "date", "ed", "seq")
-      .join(files.select("id", "file"), "id")
-      .withColumn("pages", array(struct('file as "id", 'seq, 'width, 'height, 'dpi, 'regions)))
-      .drop("file", "width", "height", "dpi", "regions")
-      .write.save(args(2))
+      .write.save(args(1))
     spark.stop()
   }
 }
