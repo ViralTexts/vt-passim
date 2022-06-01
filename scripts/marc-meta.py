53bdd6f800816f985367d3fc9d69b2bfc11225de
David Smith <dasmiq@gmail.com>
2022-06-01 08:46:02 -0400

    Get date sequence information; overwrite old output.

--- scripts/marc-meta.py
+++ scripts/marc-meta.py
@@ -1,6 +1,6 @@
 import argparse
 from pyspark.sql import SparkSession, Row, Window
-from pyspark.sql.functions import (col, concat, lit, size, udf, struct, length, substring)
+from pyspark.sql.functions import (col, concat, explode, lit, size, udf, struct, length, substring)
 from pyspark.sql.functions import regexp_replace as rr
 import pyspark.sql.functions as f
 
@@ -13,11 +13,35 @@ def subfield1(data, tag, code):
             res = subs[0].strip(':;, ')
     return res
 
+def isubfield1(data, tag, ind1, code):
+    fields = [d.subfield for d in data if d._tag == tag and d._ind1 == ind1]
+    res = None
+    if fields and len(fields) > 0:
+        subs = [s._VALUE for s in fields[0] if s._code == code]
+        if subs:
+            res = subs[0].strip(':;, ')
+    return res
+
 def field1(data, tag):
     fields = [d.subfield for d in data if d._tag == tag]
     res = None
     if fields and len(fields) > 0:
         res = ' '.join([s._VALUE for s in fields[0]])
+    return res
+
+def getLink(subfield):
+    fields = [s['_VALUE'] for s in subfield
+              if s['_code'] == 'w' and s['_VALUE'].startswith('(DLC)')]
+    res = None
+    if fields and len(fields) > 0:
+        res = '/lccn/' + fields[0].replace('(DLC)', '').replace(' ', '')
+    return res
+
+def getRelations(data):
+    fields = [(d._tag,int(d['_ind2']), getLink(d.subfield)) for d in data if d._tag in [780, 785]]
+    res = None
+    if fields and len(fields) > 0:
+        res = fields
     return res
 
 if __name__ == '__main__':
@@ -34,6 +58,7 @@ if __name__ == '__main__':
     raw = spark.read.load(config.inputPath)
 
     get_subfield1 = udf(lambda d, tag, code: subfield1(d, tag, code))
+    get_isubfield1 = udf(lambda d, tag, ind1, code: isubfield1(d, tag, ind1, code))
     get_field1 = udf(lambda d, tag: field1(d, tag))
     norm_link = udf(lambda u: u.replace('http://chroniclingamerica.loc.gov/', 'http://www.loc.gov/chroniclingamerica/').replace('http://loc.gov/', 'http://www.loc.gov/').rstrip('/ ') if u else None)
 
@@ -48,9 +73,11 @@ if __name__ == '__main__':
                       # get_field1('datafield', lit(321)).alias('formerFrequency'),
                       get_subfield1('datafield', lit(338), lit('a')).alias('carrier'),
                       # get_field1('datafield', lit(43)).alias('geocode'),
-                      get_subfield1('datafield', lit(752), lit('a')).alias('country'),
-                      get_subfield1('datafield', lit(752), lit('b')).alias('div1'),
-                      get_subfield1('datafield', lit(752), lit('c')).alias('div2'),
+                      get_isubfield1('datafield', lit(362), lit('0'), lit('a')).alias('dateseq'),
+                      get_isubfield1('datafield', lit(362), lit('1'), lit('a')).alias('udateseq'),
+                      rr(get_subfield1('datafield', lit(752), lit('a')), '\.$', '').alias('country'),
+                      rr(get_subfield1('datafield', lit(752), lit('b')), '\.$', '').alias('div1'),
+                      rr(get_subfield1('datafield', lit(752), lit('c')), '\.$', '').alias('div2'),
                       rr(get_subfield1('datafield', lit(752), lit('d')), '\.$', '').alias('city'),
                       rr(rr(rr(get_subfield1('datafield', lit(856), lit('u')),
                                'https?://chroniclingamerica.loc.gov/',
@@ -68,8 +95,22 @@ if __name__ == '__main__':
                     ).drop('marc8'
                     )
 
+    get_relations = udf(lambda d: getRelations(d),
+                        'array<struct<type: int, subtype: int, link: string>>')
+
+    links = raw.select(concat(lit('/lccn/'),
+                              f.translate(get_subfield1('datafield', lit(10), lit('a')),
+                                          ' ', '')).alias('series'),
+                       explode(get_relations('datafield')).alias('link')
+                ).select('series', 'link.*'
+                ).filter(col('link').isNotNull())
+
     # flat.write.json(config.outputPath)
-    flat.sort('series').coalesce(1).write.csv(config.outputPath, header=True, escape='"')
+    flat.sort('series').coalesce(1).write.csv(config.outputPath + '/meta',
+                                              header=True, escape='"', mode='overwrite')
+
+    links.sort('series').coalesce(1).write.csv(config.outputPath + '/links',
+                                               header=True, escape='"', mode='overwrite')
 
     spark.stop()
     
