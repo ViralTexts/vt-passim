1dbadcc1a728dd8951706d16c93f59bf3d43bc0f
David Smith <dasmiq@gmail.com>
2022-06-06 08:19:26 -0400

    Merge JSON METS data and remove duplicates.

---
+++ scripts/mets-merge.py
@@ -0,0 +1,21 @@
+import argparse
+from pyspark.sql import SparkSession, Row
+from pyspark.sql.functions import col, collect_list, explode, sort_array, struct, xxhash64
+import pyspark.sql.functions as f
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='METS merge',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('inputPath', metavar='<input path>', help='input path')
+    parser.add_argument('outputPath', metavar='<output path>', help='output path')
+
+    config = parser.parse_args()
+
+    spark = SparkSession.builder.appName('METS merge').getOrCreate()
+
+    raw = spark.read.json(config.inputPath, recursiveFileLookup='true', pathGlobFilter='*.json.gz')
+
+    raw.filter(col('series').isNotNull()).dropDuplicates(['file']).write.json(config.outputPath)
+
+    spark.stop()
+    
