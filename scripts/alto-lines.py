92d4c4689aba20a2a5e7526f3fa791ab0dd3d708
David Smith <dasmiq@gmail.com>
2023-05-25 11:06:33 -0400

    Add word-level region information if present.

--- scripts/alto-lines.py
+++ scripts/alto-lines.py
@@ -10,21 +10,36 @@ ns = {'alto': 'http://www.loc.gov/standards/alto/ns-v4#'}
 def textLines(s):
     text = ''
     lines = []
+    pages = []
     if s != None and s != '':
         tree = etree.parse(BytesIO(s.encode()))
-        for line in tree.findall('//alto:TextLine', namespaces=ns):
-            ## This should be generalized to support lines w/ and w/o CONTENT attribute.
-            start = len(text)
-            for w in line.findall('.//alto:String', namespaces=ns):
-                if text != '' and not text[len(text)-1].isspace():
-                    text += ' '
-                text += w.get('CONTENT')
-            text += '\n'
-            lines.append((start, len(text), line.get('ID')))
+        img = tree.findtext('//alto:sourceImageInformation/alto:fileName', namespaces=ns).strip()
+        seq = 0
+        for p in tree.findall('//alto:Page', namespaces=ns):
+            regions = []
+            for line in p.findall('.//alto:TextLine', namespaces=ns):
+                ## This should be generalized to support lines w/ and w/o CONTENT attribute.
+                start = len(text)
+                for w in line.findall('.//alto:String', namespaces=ns):
+                    if text != '' and not text[len(text)-1].isspace():
+                        text += ' '
+                    sstart = len(text)
+                    text += w.get('CONTENT')
+                    if w.get('HPOS') != None:
+                        regions.append((sstart, len(text) - sstart,
+                                        (int(w.get('HPOS')), int(w.get('VPOS')),
+                                         int(w.get('WIDTH')), int(w.get('HEIGHT')),
+                                         int(w.get('HEIGHT')))))
+                text += '\n'
+                lines.append((start, len(text) - start, line.get('ID')))
+            pages.append((img,
+                          int(p.get('PHYSICAL_IMG_NR', 0)),
+                          int(p.get('WIDTH', 0)), int(p.get('HEIGHT', 0)),
+                          regions))
         
-    return (text, lines)
+    return (text, lines, pages)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Alto lines',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('inputPath', metavar='<input path>', help='input path')
@@ -37,15 +52,15 @@ if __name__ == "__main__":
         ).set('mapreduce.input.fileinputformat.input.dir.recursive', 'true')
 
     text_lines = udf(lambda s: textLines(s),
-                     'struct<text: string, lineIDs: array<struct<start: int, length: int, id: string>>>').asNondeterministic()
+                     'struct<text: string, lineIDs: array<struct<start: int, length: int, id: string>>, pages: array<struct<id: string, seq: int, width: int, height: int, regions: array<struct<start: int, length: int, coords: struct<x: int, y: int, w: int, h: int, b: int>>>>>>').asNondeterministic()
 
     # spark.read.text(config.inputPath, wholetext='true', recursiveFileLookup='true'
     spark.sparkContext.wholeTextFiles(config.inputPath
         ).map(lambda f: Row(id=f[0], value=f[1])
         ).toDF(
-        ).withColumn('lines', text_lines('value')
+        ).withColumn('info', text_lines('value')
         # ).select(f.input_file_name().alias('id'), text_lines('value').alias('lines')
-        ).select('id', 'lines.*'
+        ).select('id', 'info.*'
         ).write.json(config.outputPath)
 
     spark.stop()
