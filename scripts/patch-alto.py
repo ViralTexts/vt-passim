45db11c734b35151288ef44a6b3e13f315e5dd4e
David Smith <dasmiq@gmail.com>
2023-07-21 10:57:29 -0400

    Add --lines option to select top lines.

--- scripts/patch-alto.py
+++ scripts/patch-alto.py
@@ -1,6 +1,6 @@
 import argparse, os
 from pyspark.sql import SparkSession, Row
-from pyspark.sql.functions import col, collect_list, explode, length, sort_array, struct, udf
+from pyspark.sql.functions import col, collect_list, explode, slice, sort_array, struct, udf
 import pyspark.sql.functions as f
 from io import StringIO, BytesIO
 from lxml import etree
@@ -8,7 +8,7 @@ from lxml import etree
 ns = {'alto': 'http://www.loc.gov/standards/alto/ns-v4#'}
 nspref = f'{{{ns["alto"]}}}'
 
-def patchAlto(fname, outPath, recs):
+def patchAlto(fname, path, recs):
     patch = {}
     for r in recs:
         patch[r.lineID] = r.srcText
@@ -39,7 +39,6 @@ def patchAlto(fname, outPath, recs):
             if line.get('HEIGHT') != None:
                 attr['HEIGHT'] = line.get('HEIGHT')
 
-    path = outPath + '/' + fname
     os.makedirs(os.path.dirname(path), exist_ok=True)
     tree.write(path, encoding='UTF-8', xml_declaration=True)
     return len(patch)
@@ -49,7 +48,10 @@ if __name__ == '__main__':
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-f', '--filter', type=str,
                         default='matchRate > 0.5 AND leadGap = 0 AND tailGap = 0 AND maxGap < 4',
-                         help='Filter training lines')
+                        help='Filter training lines')
+    parser.add_argument('-n', '--lines', type=int,
+                        default=10000,
+                        help='Maximum number of lines per book')
     parser.add_argument('inputPath', metavar='<input path>', help='input path')
     parser.add_argument('outputPath', metavar='<output path>', help='output path')
 
@@ -57,14 +59,24 @@ if __name__ == '__main__':
     spark = SparkSession.builder.appName('Patch Alto').getOrCreate()
     spark.sparkContext.setLogLevel('WARN')
 
-    patch_alto = udf(lambda fname, lines: patchAlto(fname, config.outputPath, lines),
+    patch_alto = udf(lambda fin, fout, lines: patchAlto(fin, config.outputPath+'/'+fout, lines),
                      'int').asNondeterministic()
 
-    lines = spark.read.json(config.inputPath
-                ).filter(config.filter
+    raw = spark.read.json(config.inputPath).filter(config.filter)
+
+    sample = raw.withColumn('book', f.regexp_replace('id', '/[^/]+$', '')
+                ).groupBy('book'
+                ).agg(slice(sort_array(collect_list(struct('matchRate', 'id', 'lineID')), False),
+                            1, config.lines).alias('lines')
+                ).select(explode('lines').alias('line')
+                ).select(col('line.id'), col('line.lineID'))
+
+    lines = raw.join(sample, ['id', 'lineID'], 'left_semi'
                 ).groupBy('id'
                 ).agg(collect_list(struct('lineID', 'srcText')).alias('lines')
-                ).select(patch_alto('id', 'lines').alias('result')
+                ).withColumn('fname', f.concat(f.monotonically_increasing_id().cast('string'),
+                                               f.lit('.xml'))
+                ).select(patch_alto('id', 'fname', 'lines').alias('result')
                 ).select(f.sum('result').alias('lines')).collect()
 
     print('# lines: ', lines)
