f5749c8f84406092349ba90c94c5bea361d76c24
David Smith <dasmiq@gmail.com>
2024-05-21 13:48:01 -0400

    Compute daylag since last issue.

--- scripts/c19.py
+++ scripts/c19.py
@@ -1,10 +1,17 @@
 import argparse
 from pyspark.sql import SparkSession
+from pyspark.sql import Window
 from pyspark.sql.functions import broadcast, coalesce, col, struct, max, min
+import pyspark.sql.functions as f
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Select C19',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--min-year', type=int, default=0,
+                        help='Minimum year')
+    parser.add_argument('--max-year', type=int, default=1900,
+                        help='Maximum year')
     parser.add_argument('inputPath', metavar='<path>', help='input path')
     parser.add_argument('groupPath', metavar='<path>', help='group path')
     parser.add_argument('outputPath', metavar='<path>', help='output path')
@@ -15,15 +22,20 @@ if __name__ == "__main__":
 
     groups = spark.read.csv(config.groupPath, header=True)
     raw = spark.read.load(config.inputPath, mergeSchema=True)
-    df = raw.filter(col('date') < '1900')
+    df = raw.withColumn('day', col('date').cast('date')
+            ).drop('group', 'daylag'
+            ).filter( (f.year('day') >= config.min_year) & (f.year('day') < config.max_year) )
 
     spark.conf.set('spark.sql.adaptive.enabled', 'true')
 
-    issues = df.groupBy('series', 'date'
+    window = Window.partitionBy('series').orderBy('day')
+    issues = df.groupBy('series', 'day'
                 ).agg(min(struct(~col('open').cast('boolean'),
-                                 col('corpus')))['corpus'].alias('corpus'))
+                                 col('corpus')))['corpus'].alias('corpus')
+                ).withColumn('daylag', f.datediff('day', f.lag('day').over(window))
+                ).na.fill(0, 'daylag')
 
-    df.join(broadcast(issues), ['series', 'date', 'corpus'], 'left_semi'
+    df.join(broadcast(issues), ['series', 'day', 'corpus']
         ).join(broadcast(groups), 'series', 'left_outer'
         ).withColumn('group', coalesce('group', 'series')
         ).write.partitionBy('open', 'corpus').save(config.outputPath)
