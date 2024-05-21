cf5db7cea23b154b331cb6519c9e9156643dbcdb
David Smith <dasmiq@gmail.com>
2024-05-21 09:32:29 -0400

    Add ca-page-merge.py to match CA METS and Alto.

---
+++ scripts/ca-page-merge.py
@@ -0,0 +1,80 @@
+import argparse
+from os.path import basename, splitext
+from pyspark.sql import SparkSession, Row, Window
+from pyspark.sql.functions import (broadcast, col, collect_list, explode, lit, size, udf, struct,
+                                   sort_array, translate, when)
+import pyspark.sql.functions as f
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Merge METS and Alto',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('inputPath', metavar='<path>', help='input path')
+    parser.add_argument('metsPath', metavar='<path>', help='path to METS data')
+    parser.add_argument('outputPath', metavar='<path>', help='output path')
+
+    config = parser.parse_args()
+
+    spark = SparkSession.builder.appName('Merge METS and Alto').getOrCreate()
+
+    file_stem = udf(lambda path: splitext(basename(path))[0] if path != None else None)
+
+    alto = spark.read.load(config.inputPath
+            ).withColumn('sourceFrame', file_stem(translate('sourceFile', '\\_', '//'))
+            ).withColumnRenamed('width', 'altoWidth'
+            ).withColumnRenamed('height', 'altoHeight'
+            ).drop('issue' #, 'text', 'regions'
+            ).dropDuplicates(['batch', 'series', 'date', 'ed', 'seq', 'sourceFrame'])
+
+    # alto.coalesce(200).write.save(config.outputPath + '/alto', mode='ignore')
+    # alto = spark.read.load(config.outputPath + '/alto')
+
+    mets = spark.read.json(config.metsPath).withColumnRenamed('file', 'metsFile'
+                ).select('*', f.posexplode('pages')
+                ).select('*', 'col.*', 'col.image.*'
+                ).drop('pages', 'col', 'image'
+                ).withColumn('frame', file_stem('file')
+                ).withColumn('series', f.trim(f.lower('series'))
+                ).withColumn('seq', col('seq').cast('int'))
+
+    mets.cache()
+
+    # mets.write.save(config.outputPath + '/mets', mode='ignore')
+    # mets = spark.read.load(config.outputPath + '/mets')
+
+    mc = mets.filter(col('file').isNotNull()
+            ).groupBy('batch', 'series', 'date', 'ed', 'seq'
+            ).agg(f.count('metsFile').alias('mets'),
+                  sort_array(collect_list(struct('frame', 'pos', 'metsFile'))).alias('frames'))
+
+    ac = alto.groupBy('batch', 'series', 'date', 'ed', 'seq'
+            ).agg(f.count('sourceFile').alias('alto'),
+                  sort_array(collect_list('sourceFrame')).alias('sourceFrame'))
+    
+    jc = mc.join(ac, ['batch', 'series', 'date', 'ed', 'seq']
+            ).withColumn('pair', explode(f.arrays_zip('frames', 'sourceFrame'))
+            ).select('batch', 'series', 'date', 'ed', 'seq', 'pair.frames.*', 'pair.sourceFrame')
+
+    linked = mets.join(jc.select('metsFile', 'pos', 'sourceFrame'),
+                       ['metsFile', 'pos'], 'left_outer'
+                ).withColumnRenamed('metsFile', 'issue')
+
+    ## Don't deduplicate issues until here, since we want a 1-to-1 mets-alto match
+    dedup = linked.withColumn('alto', col('sourceFrame').isNotNull().cast('int')
+                ).groupBy('series', 'date', 'ed', 'issue'
+                ).agg(f.sum('alto').alias('alto'), f.count('pos').alias('pp')
+                ).groupBy('series', 'date', 'ed'
+                ).agg((f.max(struct('alto', 'pp', 'issue'))['issue']).alias('issue'))
+
+    linked.join(broadcast(dedup), ['series', 'date', 'ed', 'issue'], 'left_semi'
+        ).join(alto, ['batch', 'series', 'date', 'ed', 'seq', 'sourceFrame'], 'left_outer'
+        ).withColumn('id', f.concat('issue', lit('#pageModsBib'), (col('pos') + 1))
+        ).withColumn('pages', f.array(struct(col('file').alias('id'),
+                                             'seq',
+                                             col('altoWidth').alias('width'),
+                                             col('altoHeight').alias('height'),
+                                             'dpi', 'regions')) # add regions
+        ).drop('altoWidth', 'altoHeight', 'dpi', 'file', 'regions'
+        ).write.save(config.outputPath, mode='overwrite')
+
+    spark.stop()
+    
