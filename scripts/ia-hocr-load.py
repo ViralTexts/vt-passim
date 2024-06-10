a669eaba76d7d865b10763208c9128ceaef95e31
David Smith <dasmiq@gmail.com>
2024-06-10 14:38:36 -0400

    Handle XML parsing exceptions; specify data frame schema.

--- scripts/ia-hocr-load.py
+++ scripts/ia-hocr-load.py
@@ -1,4 +1,4 @@
-import argparse, os, re
+import argparse, os, re, sys
 from pyspark.sql import SparkSession, Row
 from pyspark.sql.functions import col, collect_list, explode, sort_array, struct, udf
 import pyspark.sql.functions as f
@@ -9,14 +9,16 @@ ns = {'': 'http://www.w3.org/1999/xhtml'}
 
 def parseHOCR(path, content):
     res = []
-    if content != None and content != '':
-        dir = os.path.basename(os.path.dirname(path))
-        file = re.sub(r'_hocr.html$', '', os.path.basename(path))
-        if dir != file:
-            book = dir + '/' + file
-        else:
-            book = file
-        tree = etree.parse(BytesIO(content))
+    if content == None or content == '':
+        return res
+    dir = os.path.basename(os.path.dirname(path))
+    file = re.sub(r'_hocr.html(.gz)?$', '', os.path.basename(path))
+    if dir != file:
+        book = dir + '/' + file
+    else:
+        book = file
+    try:
+        tree = etree.parse(BytesIO(content.encode()))
         uid = 0
         for page in tree.findall("//div[@class='ocr_page']", namespaces=ns):
             text = ''
@@ -30,7 +32,12 @@ def parseHOCR(path, content):
             for par in page.findall(".//p[@class='ocr_par']", namespaces=ns):
                 for line in par.findall(".//span[@class='ocr_line']", namespaces=ns):
                     start = len(text)
+                    line_start = True
                     for w in line.findall(".//span[@class='ocrx_word']", namespaces=ns):
+                        if line_start:
+                            line_start = False
+                        else:
+                            text += ' '
                         text += w.findtext('.', namespaces=ns)
                     m = re.search(r'bbox (\d+) (\d+) (\d+) (\d+)',
                                   line.attrib.get('title', ''))
@@ -41,11 +48,19 @@ def parseHOCR(path, content):
                     text += '\n'
                 text += '\n'
             seq = int(re.sub(r'^.*_0*(\d+)', r'\1', page.attrib.get('id', '')))
+            m = re.search(r'image "(?:/tmp/)?([^"]+)"', page.attrib.get('title', ''))
+            if m:
+                imfile = '/'.join([dir, file + '_jp2.zip', m.group(1).replace('/', '%2F')])
+            else:
+                imfile = book + ('_%04d' % seq)
             res.append(Row(id=book + ('_%04d' % uid), book=book, seq=seq, text=text,
-                           pages=[Row(id=book + ('_%04d' % seq), seq=seq,
+                           pages=[Row(id=imfile, seq=seq,
                                       width=width, height=height, dpi=dpi, regions=regions)]))
             uid += 1
-            
+    except:
+        print('# Error parsing ' + path, file=sys.stderr)
+        res = []
+
     return res
 
 if __name__ == '__main__':
@@ -61,10 +76,12 @@ if __name__ == '__main__':
 
     spark = SparkSession.builder.appName('IA hOCR import').getOrCreate()
 
-    spark.read.load(config.inputPath, format='binaryFile', pathGlobFilter='*.html',
-                    recursiveFileLookup='true'
-        ).rdd.flatMap(lambda r: parseHOCR(r.path, r.content)
-        ).toDF(
-        ).write.json(config.outputPath, mode='overwrite')
+    spark.sparkContext._jsc.hadoopConfiguration().set(
+        'mapreduce.input.fileinputformat.input.dir.recursive', 'true')
+    spark.sparkContext.wholeTextFiles(config.inputPath,
+                                      minPartitions=200
+        ).flatMap(lambda f: parseHOCR(f[0], f[1])
+        ).toDF('struct<id: string, book: string, seq: int, text: string, pages: array<struct<id: string, seq: int, width: int, height: int, dpi: int, regions: array<struct<start: int, length: int, coords: struct<x: int, y: int, w: int, h: int, b: int>>>>>>'
+        ).write.save(config.outputPath, mode='overwrite')
 
     spark.stop()
