59190c98ac604a92e180952a649f24a9efbb4b73
David Smith <dasmiq@gmail.com>
2025-02-14 16:04:15 -0500

    Working version for pairwise plots.

--- scripts/cascade-paths.py
+++ scripts/cascade-paths.py
@@ -15,7 +15,7 @@ def linkLags(links):
         orig = r.src
         while orig > 0:
             cur = deps[orig]
-            res.append((cur.city, city2, r.freq, lag, exp(lp)))
+            res.append((cur.city, city2, r.year, r.freq, lag, exp(lp)))
             if cur.city in seen:
                 break
             seen.add(cur.city)
@@ -45,14 +45,21 @@ def lagStats(lags):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Lag of posteriors from cascade models',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-c', '--count', type=int, default=10,
+                        help='Minimum count', metavar='N')
+    parser.add_argument('-y', '--year', action='store_true', help='Group by year')
     parser.add_argument('inputPath', metavar='<input path>', help='input path')
     parser.add_argument('outputPath', metavar='<output path>', help='output path')
 
     config = parser.parse_args()
     spark = SparkSession.builder.appName(parser.description).getOrCreate()
 
+    covar = ['city', 'city2', 'freq']
+    if config.year:
+        covar.append('year')
+
     link_lags = udf(lambda links: linkLags(links),
-                    'array<struct<city:string, city2:string, freq:string, lag:int, p:double>>')
+        'array<struct<city:string, city2:string, year: int, freq:string, lag:int, p:double>>')
 
     lag_stats = udf(lambda lags: lagStats(lags),
                     'struct<lag25p: int, lag50p: int, lag75p: int, count: double>')
@@ -61,24 +68,23 @@ if __name__ == '__main__':
         ).filter( col('label') == 1
         # ).filter( col('city2').isNotNull()
         ).groupBy('cluster', 'dst', col('freq2').alias('freq'),
+                  f.year(f.date_add(lit('1970-01-01'),
+                                    col('day2').cast('int'))).alias('year'),
                   regexp_replace('city2', '^http://dbpedia.org/resource/', '').alias('city')
         ).agg((f.max(struct('post', 'src', 'label', 'lag'))).alias('src')
-        ).select('cluster', 'dst', 'freq', 'city', 'src.*'
+        ).select('cluster', 'dst', 'freq', 'year', 'city', 'src.*'
         # ).filter( col('src') > 0
         ).groupBy('cluster'
-        ).agg(collect_list(struct('dst', 'src', 'freq', 'city', 'lag', 'post')).alias('links')
+        ).agg(collect_list(struct('dst','src','freq','year','city','lag','post')).alias('links')
         ).select(explode(link_lags('links')).alias('lag')).select('lag.*'
         ).filter( col('city').isNotNull() & col('city2').isNotNull()
         ).filter( col('city') != col('city2')
-        ).groupBy('city', 'city2', 'freq'
+        ).groupBy(*covar
         ).agg( lag_stats(f.sort_array(collect_list(struct('lag', 'p')))).alias('stats')
-        ).select('city', 'city2', 'freq', 'stats.*'
-        ).filter( col('count') >= 1
-        ).sort(f.desc('count'), f.desc('lag50p')
-        # ).agg(f.sum(col('lag') * col('p')).alias('mlag'), f.sum('p').alias('count')
-        # ).filter( col('count') >= 10
-                  # ).withColumn('mlag', col('mlag') / col('count')
-        # ).sort(f.desc('count'), f.desc('mlag')
+        ).select(*covar, 'stats.*'
+        ).filter( col('count') >= config.count
+        ).repartition(1
+        ).sort(*covar
         ).write.csv(config.outputPath, header=True, escape='"', mode='overwrite')
 
     spark.stop()
