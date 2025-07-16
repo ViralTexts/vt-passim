a5d5b29e23949cb37002bb863760c0e0ff4abbaf
David Smith <dasmiq@gmail.com>
2025-07-16 08:52:17 -0400

    Add --dedup to grab one edition per work; split long books.

--- scripts/cts-records.py
+++ scripts/cts-records.py
@@ -1,7 +1,7 @@
 import argparse
 from unicodedata import normalize
 from pyspark.sql import SparkSession, Row
-from pyspark.sql.functions import col, collect_list, explode, length, sort_array, struct, udf
+from pyspark.sql.functions import col, collect_list, explode, length, lit, sort_array, struct, udf
 import pyspark.sql.functions as f
 
 def catLocs(recs):
@@ -15,6 +15,7 @@ def catLocs(recs):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Import CTS Records',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--dedup', action='store_true', help='Retain one edition per work.')
     parser.add_argument('inputPath', metavar='<input path>', help='input path')
     parser.add_argument('outputPath', metavar='<output path>', help='output path')
 
@@ -24,14 +25,29 @@ if __name__ == '__main__':
     nfd_norm = udf(lambda s: normalize('NFD', s))
     cat_locs = udf(lambda locs: catLocs(locs),
                    'struct<text: string, locs: array<struct<loc: string, start: int, length: int>>>')
-
-    spark.read.json(config.inputPath
-        ).filter(col('text').isNotNull()
-        ).withColumn('text', nfd_norm('text')
-        ).groupBy(col('book').alias('id')
-        ).agg(cat_locs(sort_array(collect_list(struct('seq', 'text', 'loc')))).alias('loc')
-        ).select('id', 'loc.*'
-        ).sort('id'
-        ).write.save(config.outputPath)
+    raw = spark.read.json(config.inputPath)
+    if config.dedup:
+        eds = raw.filter(f.rlike('book', lit(r'-grc')) | f.rlike('book', lit(r'-lat'))
+                ).groupBy('book'
+                ).count(
+                ).groupBy(f.regexp_replace('book', r'\.[^\.]+$', '').alias('work')
+                ).agg(f.max(struct('count', 'book')).alias('info')
+                ).select('work', col('info.book').alias('book'))
+        raw = raw.join(eds, ['book'], 'left_semi')
+        
+    raw.filter(col('text').isNotNull()
+      ).filter(f.rlike('book', lit(r'-grc')) | f.rlike('book', lit(r'-lat'))
+      ).withColumn('text', nfd_norm('text')
+      ).withColumn('parts', f.split(f.split('loc', ':')[4], r'\.')
+      ).withColumn('top', f.when((f.size('parts') >= 2) | ((f.size('parts') >= 1) & (col('book') == 'urn:cts:latinLit:stoa0119.stoa003.opp-lat1')),
+                                 f.concat(f.lit(':'), col('parts')[0])
+                                 ).otherwise(f.lit(''))
+      ).groupBy(col('book'), f.concat('book', 'top').alias('id')
+      ).agg(cat_locs(sort_array(collect_list(struct('seq', 'text', 'loc')))).alias('loc'),
+            f.min('seq').cast('int').alias('pos')
+      ).select('id', 'book', 'pos', 'loc.*'
+      ).filter(length('text')/f.size('locs') < 100000
+      ).sort('id'
+      ).write.save(config.outputPath)
 
     spark.stop()
