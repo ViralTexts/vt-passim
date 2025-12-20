3f15d8ce34856bf2110291320707d0c376e70a81
David Smith <dasmiq@gmail.com>
2025-12-20 10:33:53 -0500

    Use viewer instead of page_access; filter over-long articles.

--- scripts/trove-load.py
+++ scripts/trove-load.py
@@ -4,7 +4,7 @@ import sys
 import textwrap
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, lit, coalesce, concat, concat_ws, regexp_replace, udf
+from pyspark.sql.functions import col, lit, coalesce, concat, concat_ws, length, regexp_replace, udf
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
@@ -26,14 +26,17 @@ if __name__ == "__main__":
     raw = spark.read.json(sys.argv[1])
     # api = spark.read.json(sys.argv[2])
     
-    df = raw.na.drop(subset=['id', 'fulltext']).dropDuplicates(['id']) \
-            .select(concat(lit('trove/'), col('id')).alias('id'),
+    df = raw.na.drop(subset=['id', 'fulltext']).dropDuplicates(['id']
+           ).withColumn('viewer', concat(lit('http://trove.nla.gov.au/ndp/del/article/'), col('id'))
+           ).select(concat(lit('trove/'), col('id')).alias('id'),
                     concat_ws('/', lit('trove'), col('titleId'), col('date')).alias('issue'),
                     concat(lit('trove/'), col('titleId')).alias('series'),
                     col('date'), col('firstPageId'),
                     col('firstPageSeq').cast('int').alias('seq'),
                     col('heading').alias('title'), col('category'),
-                    text_fill(col('fulltext')).alias('text'))
+                    col('viewer'),
+                    text_fill(col('fulltext')).alias('text')
+           ).filter(length('text') < 300000) # remove long unsegmented ads
 
     # apitext = api.select(concat(lit('trove/'), col('article.id')).alias('id'),
     #                      xunesc(regexp_replace(regexp_replace(regexp_replace(
