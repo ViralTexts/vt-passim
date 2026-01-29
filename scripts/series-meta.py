0e36f44c6bb109a9e218fa316fa1ef70bd6a9c4b
David Smith <dasmiq@gmail.com>
2026-01-29 09:58:05 -0500

    Allow clobber.

--- scripts/series-meta.py
+++ scripts/series-meta.py
@@ -4,26 +4,25 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import (col, concat_ws, collect_list, sort_array)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: series-meta.py <input> <corpora> <coverage> <output>", file=sys.stderr)
+    if len(sys.argv) != 4:
+        print("Usage: series-meta.py <input> <corpora> <output>", file=sys.stderr)
         exit(-1)
     spark = SparkSession.builder.appName('Collect series metadata').getOrCreate()
 
     data = spark.read.json(sys.argv[1]
                 ).withColumn('lang', concat_ws(';', 'lang')
-                ).join(spark.read.json(sys.argv[2]
+                ).join(spark.read.csv(sys.argv[2], header=True
                             ).groupBy('series'
                             ).agg(concat_ws(';',
-                                            collect_list('corpus')).alias('corpus')),
+                                            collect_list('corpus')).alias('corpora')),
                        'series', 'left_outer'
-                ).join(spark.read.json(sys.argv[3]), 'coverage', 'left_outer'
                 ).select('series', 'title', 'lang', 'publisher', 'placeOfPublication',
-                         'subcollection', 'corpus', 'coverage', 'lon', 'lat'
+                         'corpora', 'coverage'
                 ).coalesce(1
                 ).sort('series')
 
-    data.write.json(sys.argv[4])
+    data.write.json(sys.argv[3], mode='overwrite')
 
-    data.write.csv(sys.argv[4] + '.csv', header=True, escape='"')
+    data.write.csv(sys.argv[3] + '.csv', header=True, escape='"', mode='overwrite')
 
     spark.stop()
