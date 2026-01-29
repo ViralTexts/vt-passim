48a20e7083bdc5cfbad5da7f753b8b7bc4f5f2a3
David Smith <dasmiq@gmail.com>
2026-01-29 09:32:00 -0500

    Remove common path prefix.

--- scripts/text-blocks.py
+++ scripts/text-blocks.py
@@ -1,34 +1,26 @@
-from __future__ import print_function
-from re import sub, split
-from os.path import basename, splitext
-import sys
+import argparse, os
 from pyspark.sql import SparkSession, Row
-from pyspark.sql.functions import col
-
-def fileRecords(f):
-    res = list()
-    book, ext = splitext(basename(f[0]))
-    seq = -1
-    for block in split('\n\n\n+', f[1]):
-        seq += 1
-        id = '%s#s%05d' % (book, seq)
-        res.append(Row(id=id, book=book, seq=seq,
-                       text=block.replace('&gt;', '>')\
-                                 .replace('&lt;', '<')))
-    return res
+from pyspark.sql.functions import col, concat_ws, regexp_replace, udf
+import pyspark.sql.functions as f
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: text-files.py <input> <output>", file=sys.stderr)
-        exit(-1)
+    parser = argparse.ArgumentParser(description='One text record per blank-line-delimited block',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('inputPath', metavar='<input path>', help='input path')
+    parser.add_argument('outputPath', metavar='<output path>', help='output path')
+
+    config = parser.parse_args()
+    spark = SparkSession.builder.appName(parser.description).getOrCreate()
+
+    wd = os.getcwd()
+    wd = wd if wd.startswith('file:///') else 'file://' + wd
 
     groupField = 'book'
-    spark = SparkSession.builder.appName('Load Whole Text Files').getOrCreate()
-    spark.sparkContext.setLogLevel('WARN')
-    spark.sparkContext._jsc.hadoopConfiguration()\
-                           .set('mapreduce.input.fileinputformat.input.dir.recursive', 'true')
-    spark.sparkContext.wholeTextFiles(sys.argv[1])\
-        .flatMap(lambda f: fileRecords(f))\
-        .toDF()\
-        .write.save(sys.argv[2])
+    spark.read.text(config.inputPath, recursiveFileLookup=True, wholetext=True
+        ).select(regexp_replace(f.input_file_name(), '^' + wd + '/', '').alias(groupField),
+                 f.posexplode(f.split('value', r'\n\n\n*'))
+        ).select(concat_ws('_', groupField, 'pos').alias('id'), groupField, 'pos',
+                 col('col').alias('text')
+        ).write.save(config.outputPath)
+
     spark.stop()
